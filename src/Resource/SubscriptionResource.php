147f48db7fe82550f5da0bb2abd7377d9b8225a2
Mario Tarosso <mariojgt2@gmail.com>
2024-09-07 17:16:50 +0000

    laravel-projects/packages/skeleton-store automatic push

--- src/Resource/SubscriptionResource.php
+++ src/Resource/SubscriptionResource.php
@@ -21,8 +21,10 @@ class SubscriptionResource extends JsonResource
             'id'            => $this->id,
             'plan'          => $this->plan,
             'start_date'    => $this->start_date,
+            'start_date_at' => $this->start_date->format('F j, Y'),
             'end_date'      => $this->end_date,
             'renovation_at' => $this->end_date->format('F j, Y'),
+            'status'        => $this->status,
             'duration_left' => $this->durationLeft(),
         ];
     }
